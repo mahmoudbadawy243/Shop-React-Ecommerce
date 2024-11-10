a8e0420825f0a083e1ecacafde5a08190597035e
Your Name <mahmoudbadawy243@gmail.com>
2024-11-10 20:16:49 +0200

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,57 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Cart from "./Components/Cart/Cart";
+import Products from "./Components/Products/Products";
+import Brands from "./Components/Brands/Brands";
+import Categories from "./Components/Categories/Categories";
+import Footer from "./Components/Footer/Footer";
+import Layout from "./Components/Layout/Layout";
+import Navbar from "./Components/Navbar/Navbar";
+import Register from "./Components/Register/Register";
+import Login from "./Components/Login/Login";
+import NotFound from "./Components/NotFound/Notfound";
+
+let routes = [
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "cart", element: <Cart /> },
+      { path: "products", element: <Products /> },
+      { path: "brands", element: <Brands /> },
+      { path: "categories", element: <Categories /> },
+      { path: "footer", element: <Footer /> },
+      { path: "layout", element: <Layout /> },
+      { path: "navbar", element: <Navbar /> },
+      { path: "register", element: <Register /> },
+      { path: "login", element: <Login /> },
+      { path: " * ", element: <NotFound /> },
+    ],
+  },
+];
+
+// new version of react router
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_startTransition: true, // Enable the future flag
+    v7_fetcherPersist: true,
+    v7_relativeSplatPath: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  },
+});
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider
+        router={router}
+        future={{ v7_startTransition: true }} //new version of react router
+      ></RouterProvider>
+    </>
+  );
 }
 export default App;
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+
+      <div className="container">
+        <Outlet></Outlet>
+      </div>
+
+      <Footer />
+    </>
+  );
 }
