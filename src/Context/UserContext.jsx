e064d2416199c0dc1a940972a8e5a4b3268e49f6
Your Name <mahmoudbadawy243@gmail.com>
2024-11-11 19:48:40 +0200

    reload problem > not to lohout when making reload

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -1,9 +1,14 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export let UserContext = createContext(0);
 
 export default function UserContextProvider(props) {
   const [userLogin, setUserLogin] = useState(null);
+  useEffect(() => {
+    if (localStorage.getItem("userToken") !== null) {
+      setUserLogin(localStorage.getItem("userToken"));
+    }
+  }, []);
 
   return (
     <UserContext.Provider value={{ userLogin, setUserLogin }}>
