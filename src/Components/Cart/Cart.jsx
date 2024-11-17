84996d9733ff533773889670a52a082ff2356ad9
Your Name <mahmoudbadawy243@gmail.com>
2024-11-17 10:11:09 +0200

    display and remove from cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,5 +1,100 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { CartContext } from "../../Context/CartContext";
 
 export default function Cart() {
-  return <div>Cart</div>;
+  
+  let { displayCartItems , removeCartItems , updateCartItems } = useContext(CartContext)
+  
+  
+  let [cartDetails, setCartDetails] = useState(null)  
+  async function getDisplayCartItems() {  // copry
+    let response = await displayCartItems()
+    setCartDetails(response.data);
+    
+  }
+  useEffect(()=>{ getDisplayCartItems() },[])
+// --------------------------------------------------------------------------
+
+  async function getRemoveCartItems(productId) {  // copry
+        let response = await removeCartItems(productId)
+    setCartDetails(response.data);  // to show new list
+  }
+// ----------------------------------------------------------------------------
+  async function getUpdateCartItems(productId, count) {  // copry
+    if ( count < 1) getRemoveCartItems(productId) 
+        let response = await updateCartItems(productId , count)
+    setCartDetails(response.data);  // to show new list
+  }
+  // ----------------------------------------------------------------------------
+  
+
+  return <>
+  
+<div className="relative mt-40 px-40 overflow-x-auto shadow-md sm:rounded-lg">
+  <table className=" w-full mx-auto text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <tr>
+        <th scope="col" className="px-16 py-3">
+          <span className="sr-only">Image</span>
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Product
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Qty
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Price
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Action
+        </th>
+      </tr>
+    </thead>
+        <tbody>
+          {cartDetails?.data.products.map((product) => 
+            <tr key={product.product.id}  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+              <td className="p-4">
+                <img src={product.product.imageCover} className="w-16 md:w-32 max-w-full max-h-full" alt="Apple Watch" />
+              </td>
+              <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                {product.product.title}
+              </td>
+              <td className="px-6 py-4">
+                <div className="flex items-center">
+                  <button onClick={()=>{getUpdateCartItems(product.product.id ,  product.count - 1 )}}  className="inline-flex items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
+                    <span className="sr-only">Quantity button</span>
+                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h16" />
+                    </svg>
+                  </button>
+                  <div>
+                  <span>{ product.count }</span>
+                </div>
+                  <button onClick={()=>{getUpdateCartItems(product.product.id ,  product.count + 1)}} className="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
+                    <span className="sr-only">Quantity button</span>
+                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 1v16M1 9h16" />
+                    </svg>
+                  </button>
+                </div>
+              </td>
+              <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                {product.price} EGB
+              </td>
+              <td className="px-6 py-4">
+                <span onClick={()=>{getRemoveCartItems(product.product.id)}} className="font-medium carsur:pointer text-red-600 dark:text-red-500 hover:underline">Remove</span>
+              </td>
+            </tr>
+
+          )}
+      
+    </tbody>
+  </table>
+</div>
+
+  
+  
+  
+  </>
 }
--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -12,12 +12,37 @@ async function addToCart(productId) {
     ).then((response)=>response)
     .catch((error)=>error)
 }
+// ------------------------------------------------------------------------------------------
 
+  function displayCartItems() {
+  return axios.get('https://ecommerce.routemisr.com/api/v1/cart', {
+    headers:headers
+  }).then((response) => response)
+  .catch((err)=>err)
+}
+// ------------------------------------------------------------------------------------------
 
+function removeCartItems(productId) {
+  return axios.delete( `https://ecommerce.routemisr.com/api/v1/cart/${productId}`,
+      { headers : headers}
+    ).then((response)=>response)
+    .catch((error)=>error)
+}
+// ------------------------------------------------------------------------------------------
+
+function updateCartItems(productId , count) {
+  return axios.put(`https://ecommerce.routemisr.com/api/v1/cart/${productId}`,
+    { count : count }, // body , from documentation
+    { headers : headers}
+  ).then((response)=>response)
+  .catch((error)=>error)
+}
+
+// ------------------------------------------------------------------------------------------
 export default function CartContextProvider(props) {
   
     
-    return <CartContext.Provider value={{addToCart}} >
+    return <CartContext.Provider value={{addToCart , displayCartItems , removeCartItems , updateCartItems}} >
       {props.children}
 
     </CartContext.Provider>
